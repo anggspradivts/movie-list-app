e525329c3fe076a925b08dbd2615c5c667716375
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-12-25 12:43:48 +0800

    fix: deployments error 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate"
 export default {
     darkMode: ["class"],
     content: [
@@ -59,6 +60,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
 
