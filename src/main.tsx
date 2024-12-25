0a8126f41d98578750acc98b2ee2108cce755c86
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-12-25 12:39:10 +0800

    fix: deployments error 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "@/styles/darkmode.css";
 import { RouterProvider } from "react-router-dom";
 import TanstackProvider from "./context/TanstackProvider.tsx";
 import { router } from "./router.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
