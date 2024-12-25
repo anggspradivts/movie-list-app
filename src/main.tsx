a083cd78d20cf656901059fe6f0b29eb7f68ec53
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-12-25 12:33:04 +0800

    fix: deployments error 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./styles/darkmode.css";
+import "@/styles/darkmode.css";
 import { RouterProvider } from "react-router-dom";
 import TanstackProvider from "./context/TanstackProvider.tsx";
 import { router } from "./router.tsx";
